69ba228075890e0f6e763772878ad570f9a69e9a
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2023-04-17 17:23:22 +0300

    add basename to router

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "modern-normalize/modern-normalize.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='https://ubiquitous-duckanoo-01219c.netlify.app/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
